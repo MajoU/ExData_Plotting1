813d08e1eb71186b6d21e4cc0604305bc426c160
MajoU <majo2906@gmail.com>
2014-09-01 23:27:04 +0200

    add some comments in the .R files

--- plot1.R
+++ plot1.R
@@ -3,7 +3,11 @@ library(data.table)
 # make "?" as NA values and read fast through fread
 data <- fread("household_power_consumption.txt", na.strings="?")
 
+# find row values through grep in Date column and subset
+# Global_active_power values with this rows index
 glob_pow <- data[grep("^1/2/2007|^2/2/2007", Date), Global_active_power]
+
+# create histogram
 hist(as.numeric(glob_pow), col = "red", main = "Global active power", xlab = "Global active power (kilowatts)")
 dev.copy(png, file="plot1.png", width = 480, height = 480)
 dev.off()
--- plot2.R
+++ plot2.R
@@ -5,8 +5,14 @@ data <- fread("household_power_consumption.txt", na.strings="?")
 
 sub_data <- data[grep("^1/2/2007|^2/2/2007", Date)]
 Sys.setlocale("LC_TIME", "C")
+
+# paste and assign Data and Time values into Date column
 sub_data[, Date := paste(Date, Time)]
+
+# assign to Date column time format through lubridate function
 sub_data[, Date := dmy_hms(Date)]
+
+# create plot
 plot(Global_active_power ~ Date, sub_data, xlab = " ", ylab = "Global Active Power (kilowatts)", type = "l")
 dev.copy(png, file="plot2.png", width = 480, height = 480)
 dev.off()
